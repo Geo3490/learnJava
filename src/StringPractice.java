3a13c70e187524eb2875850c9a6813e849f26363
Geo3490 <geo40127@gmail.com>
2022-07-26 18:52:54 -0400

    converting string

--- src/StringPractice.java
+++
@@ -1,7 +0,0 @@
-public class StringPractice {
-
-    public static void main(String[] args) {
-        String number = "1";
-        System.out.println();
-    }
-}
---
+++ src/com/javaLearn/Main.java
@@ -0,0 +1,12 @@
+package com.javaLearn;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String x = "1";
+
+        int y = Integer.parseInt(x) + 2;
+
+        System.out.println(y);
+    }
+}
