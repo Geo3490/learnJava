d0c84efbf82b8505180c2794fb1f974fb8a75662
Geo3490 <geo40127@gmail.com>
2022-07-26 19:19:32 -0400

    Math Practice

--- src/com/javaLearn/Main.java
+++ src/com/javaLearn/Main.java
@@ -2,11 +2,22 @@ package com.javaLearn;
 
 public class Main {
 
+//    public static void main(String[] args) {
+//        String x = "1";
+//
+//        int y = Integer.parseInt(x) + 2;
+//
+//        System.out.println(y);
+//    }
+
     public static void main(String[] args) {
-        String x = "1";
 
-        int y = Integer.parseInt(x) + 2;
+                //Math.random is by itself .000-1 that is why you multiply by 100 to get 0-100//
+int result = (int)(Math.random() * 100);
 
-        System.out.println(y);
+        System.out.println(result);
     }
+
+
 }
+
---
+++ src/com/javaLearn/Talk.java
@@ -0,0 +1,10 @@
+package com.javaLearn;
+
+public class Talk {
+
+    public static void main(String[] args) {
+
+        String myName = "George Elefsiades";
+        String g = new String();
+    }
+}
