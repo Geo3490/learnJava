0bbb04f96a0f34a0ded3171a0867c50faf5fb1ec
Geo3490 <geo40127@gmail.com>
2022-07-26 19:44:34 -0400

    number format

--- src/com/javaLearn/Main.java
+++ src/com/javaLearn/Main.java
@@ -1,9 +1,12 @@
 package com.javaLearn;
 
+import java.text.NumberFormat;
+
 public class Main {
-            //CONVERT STRING VALUE TO NUMBER VALUE USING Integer.parseInt
+    //CONVERT STRING VALUE TO NUMBER VALUE USING Integer.parseInt
 
 //    public static void main(String[] args) {
+
 //        String x = "1";
 //
 //        int y = Integer.parseInt(x) + 2;
@@ -11,14 +14,25 @@ public class Main {
 //        System.out.println(y);
 //    }
 
+    //public static void main(String[] args) {
+
+    //Math.random is by itself .000-1 this is why you multiply by 100 to get 0-100 result//
+//                int result = (int)(Math.random() * 100);
+//
+//        System.out.println(result);
+    //}
+
+    //NUMBER FORMATTING
+
+    // CONVERTING NUMBERS INTO STRING Currency $1.00 numbers//
     public static void main(String[] args) {
 
-                //Math.random is by itself .000-1 this is why you multiply by 100 to get 0-100 result//
-int result = (int)(Math.random() * 100);
+        NumberFormat money = NumberFormat.getCurrencyInstance();
+        String result = money.format(121324);
 
         System.out.println(result);
     }
-
-
 }
+
+
 
