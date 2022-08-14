5050afa8de228d4a25482c7a086a8d15b19cef14
Geo3490 <geo40127@gmail.com>
2022-08-14 10:09:29 -0400

    practice poly

--- src/com/javaLearn/Main.java
+++ src/com/javaLearn/Main.java
@@ -1,11 +1,27 @@
 package com.javaLearn;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-Animal animal;
+        Scanner scanner = new Scanner(System.in);
+        Animal animal;
 
+        System.out.println("What do You Choose for choice ?? ");
+        System.out.println("Choose 1 = Dog || Choose 2 = Cat ");
+        int choices = scanner.nextInt();
 
+        if (choices == 1){
+            animal = new Dog();
+            animal.speak();
 
+        }else if(choices == 2) {
+            animal = new Cat();
+            animal.speak();
+        }else{
+            System.out.println("Invalid Choose Again");
         }
+
     }
+}
